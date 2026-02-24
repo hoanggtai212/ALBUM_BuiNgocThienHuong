213bb44c2c871ceed7c43200980feef6dff066ee
HT <rongchube99@gmail.com>
2026-02-24 23:59:35 +0700

    Update script.js

--- style/script.js
+++ style/script.js
@@ -165,15 +165,15 @@ pages.forEach((page) => {
       if (page === pages[pages.length - 2] && !typed) {
         const endText = document.getElementById('ending-text');
         const content = `Toi iu three (3) thứ trên thế giới này: 
-        Mặt trời ☀️, 
-        Mặt trăng 🌕,
-        Và em ❤️. 
+        Mặt trời (the Sun) ☀️, 
+        Mặt trăng (the Moon)🌕,
+        Và em (the Exception) ❤️. 
         Mặt trời là ánh sáng của ban mai ✨,
         Mặt trăng là vẻ đẹp của màn đêm 🌙,
         Còn em là 🤔 ... 
         Là đệ cụa toi 😎
         Ý nhầm, còn e là 🤔 ... 
-        Là điều ngọt ngào nhất của anh 😘`;
+        Là điều ngọt ngào nhất của a 😘`;
         endText.innerHTML = "";
         typewriterEffect(content, endText);
 
@@ -208,6 +208,7 @@ pages.forEach((page) => {
   });
 
 });
+
 
 
 
